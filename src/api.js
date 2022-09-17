165b8b5707697f5d390de4547db53d0c60df25f1
aqil-far <95259816+aqil-far@users.noreply.github.com>
2022-09-17 15:02:01 -0400

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fb62a1fcd7msh368e1ff2051ca74p11f4d1jsn3bcc4c272464',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const geo_api_url = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "efd8ef502b44e9b4ad306949ba62394c";
+
